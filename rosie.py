c185f9a69210642b8429e54d153ccc0b3f6dcca1
Elijah Williams <williae2@rose-hulman.edu>
2022-04-26 15:40:48 -0400

    have a simple scapy script up an running, added docker

--- rosie.py
+++ rosie.py
@@ -1,1 +1,20 @@
+#! /usr/bin/env python3
+import scapy.all as scapy
+import sys
+#import os
+import time
+
+
+
+request = scapy.ARP()
+
+request.pdst = '172.17.0.1/24'
+broadcast = scapy.Ether()
+
+broadcast.dst = 'ff:ff:ff:ff:ff:ff'
+
+request_broadcast = broadcast / request
+clients = scapy.srp(request_broadcast, timeout=1)[0]
+for element in clients:
+    print(element[1].psrc + "     " + element[1].hwsrc)
 
